f5a1429439ec3650008581c4cc0444c6fa3d2194
HPR1999 <hpr1999@proton.me>
2025-12-04 00:49:02 +0100

    chore: scaffold day three

---
+++ 2025/aoc_elixir/lib/day_three.ex
@@ -0,0 +1,16 @@
+defmodule DayThree do
+  @moduledoc """
+  Day Three of AOC
+  """
+
+  @doc """
+
+  ## Examples
+
+  iex> DayThree.hello
+  :world
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2025/aoc_elixir/test/day_three_test.exs
@@ -0,0 +1,4 @@
+defmodule DayThreeTest do
+  use ExUnit.Case
+  doctest DayThree
+end
