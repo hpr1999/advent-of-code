6bf27681dc1a6e89d802ccd88a57c6391c4639f1
HPR1999 <hpr1999@proton.me>
2025-12-04 03:12:33 +0100

    refactor: rename, clarify and simplify day 3 solution

--- 2025/aoc_elixir/lib/day_three.ex
+++ 2025/aoc_elixir/lib/day_three.ex
@@ -1,46 +1,45 @@
 defmodule DayThree do
-  def calc_joltage(bat_bank, num_batteries) when is_integer(bat_bank) do
-    calc_joltage(Integer.digits(bat_bank), num_batteries, 0)
+  def joltage(battery_bank, num_batteries) when is_integer(battery_bank) do
+    joltage(Integer.digits(battery_bank), num_batteries, 0)
   end
 
-  defp calc_joltage(bat_bank, num_batteries, sum)
-       when is_list(bat_bank) and num_batteries >= 0 and sum >= 0 do
+  defp joltage(battery_bank, num_batteries, sum)
+       when is_list(battery_bank) and num_batteries >= 0 and sum >= 0 do
+    # when we only need one more battery, just get the biggest that's left
     if num_batteries == 1 do
-      sum + Enum.max(bat_bank)
+      sum + Enum.max(battery_bank)
     else
-      [num | rest] = find_max_digit_with_trail(bat_bank, num_batteries)
-      calc_joltage(rest, num_batteries - 1, sum + num * Integer.pow(10, num_batteries - 1))
+      [num | rest] = largest_digit_sequence(battery_bank, num_batteries)
+      battery_value = num * Integer.pow(10, num_batteries - 1)
+      joltage(rest, num_batteries - 1, sum + battery_value)
     end
   end
 
-  def find_max_digit_with_trail(digits, min_trail_size) when is_list(digits) do
-    find_max_digit_with_trail(digits, digits, min_trail_size)
+  def largest_digit_sequence(digits, min_trail_size) when is_list(digits) do
+    largest_digit_sequence(min_trail_size, digits, digits)
   end
 
-  defp find_max_digit_with_trail(remaining_digits, result_digits, min_trail_size) do
-    [max | _trail] = result_digits
+  defp largest_digit_sequence(sequence_length, digits_to_check, result) do
+    case digits_to_check do
+      # If we need all remaining digits to satisfy the length, we return
+      list when is_list(list) and length(list) < sequence_length ->
+        result
 
-    case remaining_digits do
-      list when is_list(list) and length(list) < min_trail_size ->
-        result_digits
-
-      [head | tail] when head > max ->
-        find_max_digit_with_trail(tail, remaining_digits, min_trail_size)
-
-      [head | tail] when head <= max ->
-        find_max_digit_with_trail(tail, result_digits, min_trail_size)
+      [head | tail] ->
+        result = if head > hd(result), do: digits_to_check, else: result
+        largest_digit_sequence(sequence_length, tail, result)
     end
   end
 
-  def solve_part_one(bat_banks) when is_list(bat_banks) do
-    bat_banks
-    |> Enum.map(fn bat_bank -> calc_joltage(bat_bank, 2) end)
+  def solve_part_one(battery_banks) when is_list(battery_banks) do
+    battery_banks
+    |> Enum.map(fn battery_bank -> joltage(battery_bank, 2) end)
     |> Enum.sum()
   end
 
-  def solve_part_two(bat_banks) when is_list(bat_banks) do
-    bat_banks
-    |> Enum.map(fn bat_bank -> calc_joltage(bat_bank, 12) end)
+  def solve_part_two(battery_banks) when is_list(battery_banks) do
+    battery_banks
+    |> Enum.map(fn battery_bank -> joltage(battery_bank, 12) end)
     |> Enum.sum()
   end
 
@@ -54,8 +53,10 @@ defmodule DayThree do
   def solve_part_two() do
     AocElixir.read_input(3)
     |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
-    |> solve_part_two()
+    |> Task.async_stream(fn str ->
+      joltage(String.to_integer(str), 12)
+    end)
+    |> Enum.sum_by(fn {:ok, data} -> data end)
   end
 
   def main do
--- 2025/aoc_elixir/test/day_three_test.exs
+++ 2025/aoc_elixir/test/day_three_test.exs
@@ -2,33 +2,33 @@ defmodule DayThreeTest do
   use ExUnit.Case
   doctest DayThree
 
-  test "find_max_trail" do
-    assert DayThree.find_max_digit_with_trail([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2) ===
+  test "largest_digit_sequence" do
+    assert DayThree.largest_digit_sequence([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2) ===
              [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]
 
-    assert DayThree.find_max_digit_with_trail([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 12) ===
+    assert DayThree.largest_digit_sequence([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2) ===
              [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]
 
-    assert DayThree.find_max_digit_with_trail([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 2) ===
+    assert DayThree.largest_digit_sequence([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 2) ===
              [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]
 
-    assert DayThree.find_max_digit_with_trail([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 2) ===
+    assert DayThree.largest_digit_sequence([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 2) ===
              [7, 8]
 
-    assert DayThree.find_max_digit_with_trail([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 2) ===
+    assert DayThree.largest_digit_sequence([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 2) ===
              [9, 1, 1, 1, 1, 2, 1, 1, 1]
   end
 
   test "joltage lines" do
-    assert DayThree.calc_joltage(987_654_321_111_111, 2) === 98
-    assert DayThree.calc_joltage(811_111_111_111_119, 2) === 89
-    assert DayThree.calc_joltage(234_234_234_234_278, 2) === 78
-    assert DayThree.calc_joltage(818_181_911_112_111, 2) === 92
+    assert DayThree.joltage(987_654_321_111_111, 2) === 98
+    assert DayThree.joltage(811_111_111_111_119, 2) === 89
+    assert DayThree.joltage(234_234_234_234_278, 2) === 78
+    assert DayThree.joltage(818_181_911_112_111, 2) === 92
 
-    assert DayThree.calc_joltage(987_654_321_111_111, 12) === 987_654_321_111
-    assert DayThree.calc_joltage(811_111_111_111_119, 12) === 811_111_111_119
-    assert DayThree.calc_joltage(234_234_234_234_278, 12) === 434_234_234_278
-    assert DayThree.calc_joltage(818_181_911_112_111, 12) === 888_911_112_111
+    assert DayThree.joltage(987_654_321_111_111, 12) === 987_654_321_111
+    assert DayThree.joltage(811_111_111_111_119, 12) === 811_111_111_119
+    assert DayThree.joltage(234_234_234_234_278, 12) === 434_234_234_278
+    assert DayThree.joltage(818_181_911_112_111, 12) === 888_911_112_111
   end
 
   test "part one" do
