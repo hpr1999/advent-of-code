76884699a37364469405cc70427dd82ca85279ec
HPR1999 <hpr1999@proton.me>
2025-12-04 01:52:57 +0100

    feat: solve day three part one

--- 2025/aoc_elixir/lib/day_three.ex
+++ 2025/aoc_elixir/lib/day_three.ex
@@ -1,16 +1,46 @@
 defmodule DayThree do
-  @moduledoc """
-  Day Three of AOC
-  """
+  def calc_joltage(bat_bank) when is_integer(bat_bank) do
+    [first_num | rest] = find_max_digit_with_trail(bat_bank)
+    second_num = Enum.max(rest)
 
-  @doc """
+    first_num * 10 + second_num
+  end
 
-  ## Examples
+  def find_max_digit_with_trail(int) when is_integer(int) do
+    digits = Integer.digits(int)
+    find_max_digit_with_trail(digits, digits)
+  end
 
-  iex> DayThree.hello
-  :world
-  """
-  def hello do
-    :world
+  defp find_max_digit_with_trail(remaining_digits, result_digits) do
+    [max | _trail] = result_digits
+
+    case remaining_digits do
+      [_last] -> result_digits
+      [head | tail] when head > max -> find_max_digit_with_trail(tail, remaining_digits)
+      [head | tail] when head <= max -> find_max_digit_with_trail(tail, result_digits)
+    end
+  end
+
+  def sum_joltages(bat_banks) when is_list(bat_banks) do
+    bat_banks
+    # |> Task.async_stream(&calc_joltage/1)
+    # |> Enum.sum_by(fn {:ok, joltage} -> joltage end)
+    |> Enum.map(&calc_joltage/1)
+    |> Enum.sum()
+  end
+
+  def solve_part_one() do
+    AocElixir.read_input(3)
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+    |> sum_joltages()
+  end
+
+  def solve_part_two() do
+  end
+
+  def main do
+    IO.puts(~s"Part One: #{solve_part_one()}")
+    IO.puts(~s"Part Two: #{solve_part_two()}")
   end
 end
--- 2025/aoc_elixir/test/day_three_test.exs
+++ 2025/aoc_elixir/test/day_three_test.exs
@@ -1,4 +1,34 @@
 defmodule DayThreeTest do
   use ExUnit.Case
   doctest DayThree
+
+  test "find_max_trail" do
+    assert DayThree.find_max_digit_with_trail(987_654_321_111_111) ===
+             [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1]
+
+    assert DayThree.find_max_digit_with_trail(811_111_111_111_119) ===
+             [8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]
+
+    assert DayThree.find_max_digit_with_trail(234_234_234_234_278) ===
+             [7, 8]
+
+    assert DayThree.find_max_digit_with_trail(818_181_911_112_111) ===
+             [9, 1, 1, 1, 1, 2, 1, 1, 1]
+  end
+
+  test "joltage lines" do
+    assert DayThree.calc_joltage(987_654_321_111_111) === 98
+    assert DayThree.calc_joltage(811_111_111_111_119) === 89
+    assert DayThree.calc_joltage(234_234_234_234_278) === 78
+    assert DayThree.calc_joltage(818_181_911_112_111) === 92
+  end
+
+  test "sum joltage" do
+    assert DayThree.sum_joltages([
+             987_654_321_111_111,
+             811_111_111_111_119,
+             234_234_234_234_278,
+             818_181_911_112_111
+           ]) === 357
+  end
 end
